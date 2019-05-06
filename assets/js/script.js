265b369d9bdcd2b182377e818eb113c1cbd2fd11
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-05-06 17:27:40 +0000

    Amended index.html file and custom css styles

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-//Ensures the code doesn't run until after the index.html file is loaded
-$(function () {
-    
-    //toggle the fa chevron to up/down when each accordion button is clicked
-    $("button").click(function () {
-        $(this).children('.fas').toggleClass('fa-chevron-up fa-chevron-down');
-    });
-    
-});
