306e4df75854b56d609febf46cc43960e14d89b6
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-05-07 09:26:06 +0000

    Added sound variables to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+//Ensures the code doesn't run until after the index.html file is loaded
+$(function () {
+    
+    //Toggle the fa chevron to up/down when each accordion button is clicked
+    $(".btn-link").click(function() {
+        $(this).children('.fas').toggleClass('fa-chevron-up fa-chevron-down');
+    });
+    
+    //Sound Variables
+    const yellowBtnSound = new Audio("assets/sounds/yellow-btn.mp3");
+    const redBtnSound = new Audio("assets/sounds/red-btn.mp3");
+    const greenBtnSound = new Audio("assets/sounds/green-btn.mp3");
+    const blueBtnSound = new Audio("assets/sounds/blue-btn.mp3");
+    const clickOnSound = new Audio("assets/sounds/click-on.mp3");
+    const clickOffSound = new Audio("assets/sounds/click-off.mp3");
+    const winGameSound = new Audio("assets/sounds/win-game.mp3");
+    const loseGameSound = new Audio("assets/sounds/lose-game.mp3");
+    
+
+    
+});
