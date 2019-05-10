683da222933e068eae4cf1be9661b65b165a5f30
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-05-10 14:43:48 +0000

    Cleared game logic and created new variables in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 //Ensures the code doesn't run until after the index.html file is loaded
 $(function() {
-
+    
     //Toggle the fa chevron to up/down when each accordion button is clicked
     $(".btn-link").click(function() {
         $(this).children('.fas').toggleClass('fa-chevron-up fa-chevron-down');
     });
-
+    
     //-----------------------------------------------------------Sound Variables
     const yellowBtnSound = new Audio("assets/sounds/yellow-btn.mp3");
     const redBtnSound = new Audio("assets/sounds/red-btn.mp3");
@@ -15,133 +15,31 @@ $(function() {
     const clickOffSound = new Audio("assets/sounds/click-off.mp3");
     const winGameSound = new Audio("assets/sounds/win-game.mp3");
     const loseGameSound = new Audio("assets/sounds/lose-game.mp3");
-
-    //-----------------------------------------------------Game button variables
-    let gameBtns = [
-        "yellow-btn",
-        "red-btn",
-        "green-btn",
-        "blue-btn"
-    ];
-    const powerBtn = document.getElementById("power-btn");
-    const startBtn = document.getElementById("start-btn");
-    const strictBtn = document.getElementById("strict-btn");
-    const roundNum = document.getElementById("round-num");
-
+    
+    // //--------------------------------------------------Game button variables
+    const yellowBtn = $("#yellow-btn");
+    const redBtn = $("#red-btn");
+    const greenBtn = $("#green-btn");
+    const blueBtn = $("#blue-btn");
+    const powerBtn = $("#power-btn");
+    const startBtn = $("#start-btn");
+    const strictBtn = $("#strict-btn");
+    const roundTxt = $("#round-text");
+    const roundNum = $("#round-num");
+    
     //-----------------------------------------------------------------Variables
     let cpuSequence = []; //CPU sequence
-    let count = 0; //Number of button presses
-    const totalRounds = 20; //Total number of rounds
-
-    //-----------------------------------------------------------User's sequence
-
-    //Add event listeners to each game button
-    $("#yellow-btn").click(function() {
-        checkUserInput(0);
-    });
-
-    $("#red-btn").click(function() {
-        checkUserInput(1);
-    });
-
-    $("#green-btn").click(function() {
-        checkUserInput(2);
-    });
-
-    $("#blue-btn").click(function() {
-        checkUserInput(3);
-    });
-
-    //Checks the user input value and determines if it is correct
-    function checkUserInput(id) {
-        //lights the btn and plays the sound
-        pressBtn(id);
-
-        //Validate userSequence
-        if (id == cpuSequence[count]) {
-            //Increment the count with every button press
-            count++;
-            $(roundNum).text(count);
-            if (count < totalRounds && count == cpuSequence.length) {
-                //Reset the count to 0 after each correct sequence
-                count = 0;
-                startCpuSequence();
-            }
-            else if (count == totalRounds) {
-                $(roundNum).text("CONGRATULATIONS! YOU WIN!");
-                winGameSound.play();
-            }
-        }
-        
-        
-    }
-
-
-
-    //Plays the sound and adds class when relevant game button is pressed - passes the value into checkUserInput() function
-    function pressBtn(id) {
-        playBtnSound(id);
-        $("#" + gameBtns[id]).addClass("lit");
-        //setTimeout() method background information from w3schools.com
-        setTimeout(function() {
-            $("#" + gameBtns[id]).removeClass("lit");
-        }, 500);
-    }
-
-
-
-    //------------------------------------------------------------CPU's sequence
-    $(startBtn).click(function() {
-        startCpuSequence();
-    });
-
-    function startCpuSequence() {
-        getNewMove();
-        let i = 0;
-        //setInterval() method background information from w3schools.com
-        let interval = setInterval(function() {
-            showCpuSequence(i);
-            i++;
-            if (i >= cpuSequence.length) {
-                clearInterval(interval);
-            }
-        }, 1000); //Function should execute every 1 second
-    }
-
-    //Generate random number - sample code from freecodecamp.org and tailored it to my requirements
-    function getNewMove() {
-        let newMove = Math.floor(Math.random() * 4);
-        cpuSequence.push(newMove);
-    }
-
-    //Show CPU sequence function, which adds class when button is lit and removes class when timeout interval is reached
-    function showCpuSequence(i) {
-        playBtnSound(cpuSequence[i]);
-        $("#" + gameBtns[cpuSequence[i]]).addClass("lit");
-        //setTimeout() method background information from w3schools.com
-        setTimeout(function() {
-            $("#" + gameBtns[cpuSequence[i]]).removeClass("lit");
-        }, 500);
-    }
-
-    //playSound function, which plays sounds that correspond to the particular button
-    function playBtnSound(button) {
-        switch (button) {
-            case 0:
-                yellowBtnSound.play();
-                break;
-            case 1:
-                redBtnSound.play();
-                break;
-            case 2:
-                greenBtnSound.play();
-                break;
-            case 3:
-                blueBtnSound.play();
-                break;
-        }
-    }
-
-
-
+    let userSequence = []; //User sequence
+    let flash; //Number of flashes that appear in the game
+    let round; //Determines which round/attempt we are on
+    let correct; //Determines whether the player has hit the right colors or not
+    let cpuTurn; //keeps track of whether it's the computer's turn or the user's turn
+    let intervalId; //Determines the duration of function execution
+    let strict = false; //Strict mode will be false as default
+    let sound = true; //Ensures that the sound is on when the game is being played
+    let power = false; //Game is off as a default so power variable is set to false
+    let win; //Determines if the player has won the game or not
+    
+    
+    
 });
